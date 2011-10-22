ca23ba962c9063434a9de771ea37a1a013fe78c1
Clément Turbelin <clement.turbelin@gmail.com>
2011-10-22 14:48:32 +0000

    improving example page. unit Test => phpunit 3.5
    git-svn-id: https://rserve-php.googlecode.com/svn/trunk@15 0468a86f-3dda-c865-2c5d-1fe5c1ae8f2c

--- tests/index.php
+++ tests/index.php
@@ -5,21 +5,60 @@
 require_once 'config.php';
 require '../Connection.php';
 
+$test_cases = Rserve_Tests_Definition::$native_tests;
+
+require 'example/head.php';
+
 try { 
 
-    echo 'Connecting to Rserve '.RSERVE_HOST;
+    echo '<div id="tab_0" class="tab">';
+    echo '<p>Connecting to Rserve '.RSERVE_HOST;
     $r = new Rserve_Connection(RSERVE_HOST);
+    echo ' OK</p>';
+    
+    echo '<p>Use the above menu to see results for various results using each kind of parser</p>';
+    
+    echo '</div>';
+    
+    echo '<div id="tab_1" class="tab">';
+    echo '<h2>Chi2</h2>';
+    $x = $r->evalString('a=rpois(100,100); b=rpois(100,100); list(a,b)');
 
-    $x = $r->evalString('a=rpois(100,100); b=rpois(100,100)');
-    var_dump($x);
-
+    //var_dump($x);
+    
     $x = $r->evalString('chisq.test(table(a,b))', Rserve_Connection::PARSER_REXP);
 
-    echo '<style>'.file_get_contents('rexp.css').'</style>';
     echo $x->toHTML();
-
+    echo '</div>';
+    
+    $parsers = array(
+        Rserve_Connection::PARSER_NATIVE=>'native ',
+        Rserve_Connection::PARSER_NATIVE_WRAPPED=>' wrapped native (RNative)',
+        Rserve_Connection::PARSER_DEBUG=>'Parser Debug',
+        Rserve_Connection::PARSER_REXP=>'REXP',
+    );
+    
+    $i = 1;
+    foreach($parsers as $parser=>$title) {
+        ++$i;
+        echo '<div id="tab_'.$i.'" class="tab">';
+        echo '<h2>Test Cases / '.$title.'</h2>';
+        foreach($test_cases as $test) {
+            $cmd = $test[0];
+            echo '<div class="rcmd">&gt; '.$cmd.'</div>';
+            $x = $r->evalString($cmd, $parser);
+            echo '<div class="vardump">';
+            var_dump($x);
+            echo '</div>';
+        }
+        echo '</div>';
+    }
+    
     $r->close();
 } catch(Exception $e) {
     echo $e;
 }
+
+require 'example/foot.php';
+
 ?>
