cc76862a4e48e06080899083895774a6668f7507
Clément Turbelin <clement.turbelin@gmail.com>
2011-10-22 17:59:46 +0000

    improving example page. unit Test => phpunit 3.5
    git-svn-id: https://rserve-php.googlecode.com/svn/trunk@16 0468a86f-3dda-c865-2c5d-1fe5c1ae8f2c

--- tests/ParserNativeTest.php
+++ tests/ParserNativeTest.php
@@ -1,23 +1,11 @@
 <?php
-if ( !defined("PHPUnit_MAIN_METHOD") ) {
-    define("PHPUnit_MAIN_METHOD", "ParserNativeTest::main");
-}
 
 require_once 'config.php';
-
-require_once "PHPUnit/Framework/TestCase.php";
-require_once "PHPUnit/Framework/TestSuite.php";
-
 require_once dirname(__FILE__).'/../Connection.php';
-require_once dirname(__FILE__).'/config.php';
 
 class ParserNativeTest extends PHPUnit_Framework_TestCase {
 
-    public static function main() {
-        require_once "PHPUnit/TextUI/TestRunner.php";
-        $suite  = new PHPUnit_Framework_TestSuite("ParserNativeTest");
-        $result = PHPUnit_TextUI_TestRunner::run($suite);
-    }
+    private static $cnx;
 
     /**
      * Sets up the fixture, for example, open a network connection.
@@ -26,47 +14,18 @@ class ParserNativeTest extends PHPUnit_Framework_TestCase {
      * @access protected
      */
     protected function setUp() {
-        $this->rserve = new Rserve_Connection(RSERVE_HOST);
+        if(!self::$cnx) {
+            self::$cnx = new Rserve_Connection(RSERVE_HOST);
+        }
+        $this->rserve = self::$cnx;
     }
     
     /**
     * Provider for test cases
     */
     function providerSimpleTests() {
-        return array(
-            // logical value
-            array('TRUE', 'bool', TRUE),
-            // logical vector
-            array('c(T,F,T,F,T,F,F)', 'bool', array(TRUE,FALSE,TRUE,FALSE,TRUE,FALSE,FALSE)),
-            // integer value
-            array('as.integer(12345)', 'int', 12345),
-            // integer vector
-            array('as.integer(c(34, 45, 34, 93, 604, 376, 2, 233456))', 'int', array(34,45,34,93,604,376,2, 233456)),
-            // numeric
-            array('c(34.2, 45.5, 987.2, 22.1, 87.0, 345.0, 1E-6, 1E38)', 'float', array(34.2, 45.5, 987.2, 22.1, 87.0, 345.0, 1E-6, 1E38)),
-            // character
-            array('"TOTO is TOTO"', 'string', 'TOTO is TOTO'),
-            // character vector
-            array('c("TOTO is TOTO","Ohhhh","String2")', 'string', array("TOTO is TOTO","Ohhhh","String2")),
-
-            // pairlist
-            array('list("toto"=1,"titi"=2)',NULL, array('toto'=>1,'titi'=>2)),
-
-            // pairlist
-            array('list("toto"=1,"titi"=2, "tutu"="TOTO")', NULL, array('toto'=>1,'titi'=>2,'tutu'=>'TOTO')),
-
-            // factors
-            array('factor(c("toto","titi","toto","tutu"))',NULL, array("toto","titi","toto","tutu")),
-            
-            // data.frame : Caution with data.frame, use stringsAsFactors=F 
-            array('data.frame("toto"=c(1,2,3),"titi"=c(2,2,3),"tutu"=c("foo","bar","i need some sleep"), stringsAsFactors =F)', NULL, 
-                array('toto'=>array(1,2,3),'titi'=>array(2,2,3),'tutu'=>array('foo','bar','i need some sleep')) ),
-                
-            array('chisq.test(as.matrix(c(12,58,79,52),ncol=2))[c("statistic","p.value","expected")]',NULL, array('statistic'=>46.8209, 'p.value'=>3.794258e-10,'expected'=>array(50.25,50.25,50.25,50.25)), array('statistic'=>'round|4','p.value'=>'round|16')),
-        );
-    
-    }
-      
+        return Rserve_Tests_Definition::$native_tests;
+    }    
     
     /**
     * @dataProvider providerSimpleTests
@@ -80,15 +39,15 @@ class ParserNativeTest extends PHPUnit_Framework_TestCase {
     public function testSimpleTypes($cmd, $type, $expected, $filters=NULL) {
         $r = $this->rserve->evalString($cmd);
         if( is_array($expected) ) {
-            $this->assertType('array',$r);
+            $this->assertInternalType('array',$r);
             if( is_array($r) AND !is_null($type) ) {
                 foreach($r as $x) {
-                    $this->assertType($type,$x);
+                    $this->assertInternalType($type,$x);
                 }
             }
         } else {
             if( !is_null($type) ) {
-                $this->assertType($type, $r);
+                $this->assertInternalType($type, $r);
             }
         }
         if( !is_null($filters) ) {
