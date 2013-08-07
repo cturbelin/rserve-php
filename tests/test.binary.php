3f8254eea6e34de52cc43ada48429e7d317dc1c0
Clément Turbelin <clement.turbelin@gmail.com>
2013-08-07 14:06:22 +0000

    update binary REXP testing
    git-svn-id: https://rserve-php.googlecode.com/svn/trunk@40 0468a86f-3dda-c865-2c5d-1fe5c1ae8f2c

--- tests/test.binary.php
+++ tests/test.binary.php
@@ -5,7 +5,6 @@
 */
 
 require '../Connection.php';
-require '../Binary.php';
 
 require_once 'config.php';
 
@@ -27,47 +26,59 @@ function create_REXP($values, $type, $options=array()) {
 	return $r;
 }
 
-function testBinary($values, $type, $options = array(), $msg = '') {
+function testBinary($cnx, $values, $type, $options = array(), $msg = '') {
+	echo '=================================='.CLI_EOL;
 	echo 'Test '.$type.' '.$msg.CLI_EOL;
 	
 	$tt  = strtolower($type);
 	
-	$r = create_REXP($values, $type, $options);
+	$rexp = create_REXP($values, $type, $options);
 	
-	$bin = Rserve_Parser::createBinary($r);
+	$bin = Rserve_Parser::createBinary($rexp);
 	//var_dump($bin);
 	
 	$i = 0;
+	echo "Debug REXP".CLI_EOL;
 	var_dump(Rserve_Parser::parseDebug($bin, $i));
 	
 	$i = 0;
+	echo "binary to REXP".CLI_EOL;
 	$r2 = Rserve_Parser::parseREXP($bin, $i);
 	var_dump($r2);
 	
 	$cn2 = get_class($r2);
 	
-	$cn = get_class($r);
+	$cn = get_class($rexp);
 	if( strtolower($cn2) != strtolower($cn)) {
-		echo 'Differentes classes';
+		echo 'Differentes classes'.CLI_EOL;
 		return FALSE;
 	} else {
-		echo 'Class Type ok';
+		echo 'Class Type ok'.CLI_EOL;
 	}
+	$r = $cnx->assign('x', $rexp);
+	if($r['is_error']) {
+		echo $cnx->getErrorMessage($r['error']);
+	} else {
+		echo "OK";
+	}
+	echo CLI_EOL;
+	echo "Check R object".CLI_EOL;
+	$r = $cnx->evalString('x');
+	var_dump($r);
 }
 
-testBinary(array(1,2,3), 'Integer'  );
+$cnx = new Rserve_Connection('localhost', 6311, TRUE);
 
-testBinary(array(1.1, 2.2, 3.3), 'Double'  );
+testBinary($cnx, array(1,2,3), 'Integer'  );
 
-testBinary( array(TRUE, FALSE, TRUE, NULL), 'Logical');
+testBinary($cnx, array(1.1, 2.2, 3.3), 'Double'  );
+
+testBinary($cnx, array(TRUE, FALSE, TRUE, NULL), 'Logical');
+
+testBinary($cnx, array('toto', 'titi', 'Lorem ispum', NULL), 'String');
 
 
-$rexp = create_REXP(array(1,2,3), 'Integer');
+//$rexp = create_REXP(array(1,2,3), 'Integer');
 
-$cnx = new Rserve_Binary('localhost', 6311, TRUE);
 
-$r = $cnx->assign('x', $rexp);
 
-var_dump($r);
-
-echo $cnx->getErrorMessage($r['error']);
