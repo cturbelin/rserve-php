507faf462bc8becfa79fc42a3f10ad89eb2f02a5
Clément Turbelin <clement.turbelin@gmail.com>
2016-04-17 23:45:47 +0200

    add Error fix CS

--- src/Parser/REXP.php
+++ src/Parser/REXP.php
@@ -5,6 +5,7 @@ namespace Sentiweb\Rserve\Parser;
 use Sentiweb\Rserve\Parser;
 use Sentiweb\Rserve\REXP\Complex;
 use Sentiweb\Rserve\REXP\Double;
+use Sentiweb\Rserve\REXP\Error;
 use Sentiweb\Rserve\REXP\Factor;
 use Sentiweb\Rserve\REXP\GenericVector;
 use Sentiweb\Rserve\REXP\RList;
@@ -29,8 +30,8 @@ class REXP extends Parser {
 		$i = $offset;
 		
 		// some simple parsing - just skip attributes and assume short responses
-		$ra = _rserve_int8 ( $r, $i );
-		$rl = _rserve_int24 ( $r, $i + 1 );
+		$ra = _rserve_int8( $r, $i );
+		$rl = _rserve_int24( $r, $i + 1 );
 		$i += 4;
 		
 		$offset = $eoa = $i + $rl;
@@ -40,17 +41,17 @@ class REXP extends Parser {
 		
 		if ($ra > self::XT_HAS_ATTR) {
 			$ra &= ~ self::XT_HAS_ATTR;
-			$al = _rserve_int24 ( $r, $i + 1 );
+			$al = _rserve_int24( $r, $i + 1 );
 			$tmp = $i;
-			$attr = $this->parse ( $buf, $tmp );
+			$attr = $this->parse( $buf, $tmp );
 			$i += $al + 4;
 		}
 		
-		$class = ($attr) ? $attr->at ( 'class' ) : null;
-		if ($class) {
-			$class = $class->getValues ();
-		}
+		$class = ($attr) ? $attr->at( 'class' ) : null;
 		
+		if ($class) {
+			$class = $class->getValues();
+		}
 		switch ($ra) {
 			case self::XT_NULL :
 				$a = new RNull();
@@ -59,11 +60,11 @@ class REXP extends Parser {
 			case self::XT_VECTOR : // generic vector
 				$v = array ();
 				while ( $i < $eoa ) {
-					$v [] = $this->parse ( $buf, $i );
+					$v [] = $this->parse( $buf, $i );
 				}
 				$use_df = false;
 				if ($class) {
-					if (in_array ( 'data.frame', $class )) {
+					if (in_array('data.frame', $class )) {
 						$use_df = true;
 					}
 				}
@@ -83,103 +84,105 @@ class REXP extends Parser {
 			
 			case self::XT_LIST_NOTAG :
 			case self::XT_LANG_NOTAG : // pairlist w/o tags
-				$v = array ();
+				$v = array();
 				while ( $i < $eoa ) {
-					$v [] = $this->parse ( $buf, $i );
+					$v [] = $this->parse( $buf, $i );
 				}
-				$a = ($ra == self::XT_LIST_NOTAG) ? new RList () : new Language ();
-				$a->setValues ( $a );
+				$a = ($ra == self::XT_LIST_NOTAG) ? new RList() : new Language();
+				$a->setValues( $a );
 				break;
 			
 			case self::XT_LIST_TAG :
 			case self::XT_LANG_TAG : // pairlist with tags
-				$v = array ();
-				$names = array ();
+				$v = array();
+				$names = array();
 				while ( $i < $eoa ) {
-					$v [] = $this->parse ( $buf, $i );
-					$names [] = $this->parse ( $buf, $i );
+					$v[] = $this->parse( $buf, $i );
+					$names[] = $this->parse( $buf, $i );
 				}
-				$a = ($ra == self::XT_LIST_TAG) ? new RList () : new Language ();
-				$a->setValues ( $v );
-				$a->setNames ( $names );
+				$a = ($ra == self::XT_LIST_TAG) ? new RList() : new Language();
+				$a->setValues( $v );
+				$a->setNames( $names );
 				break;
 			
 			case self::XT_ARRAY_INT : // integer array
 				$v = array ();
 				while ( $i < $eoa ) {
-					$v [] = _rserve_int32 ( $r, $i );
+					$v [] = _rserve_int32( $r, $i );
 					$i += 4;
 				}
 				$use_factor = false;
 				if ($class) {
-					if (in_array ( 'factor', $class )) {
+					if (in_array( 'factor', $class )) {
 						$use_factor = true;
 					}
 				}
-				$a = $use_factor ? new Factor () : new Integer ();
-				$a->setValues ( $v );
+				$a = $use_factor ? new Factor() : new Integer();
+				$a->setValues( $v );
 				break;
 			
 			case self::XT_ARRAY_DOUBLE : // double array
 				$v = array ();
 				while ( $i < $eoa ) {
-					$v [] = _rserve_flt64 ( $r, $i );
+					$v [] = _rserve_flt64( $r, $i );
 					$i += 8;
 				}
-				$a = new Double ();
-				$a->setValues ( $v );
+				$a = new Double();
+				$a->setValues( $v );
 				break;
 			
 			case self::XT_ARRAY_STR : // string array
-				$v = array ();
+				$v = array();
 				$oi = $i;
 				while ( $i < $eoa ) {
-					if (ord ( $r [$i] ) == 0) {
-						$v [] = substr ( $r, $oi, $i - $oi );
+					if ( ord( $r[$i] ) == 0) {
+						$v[] = substr( $r, $oi, $i - $oi );
 						$oi = $i + 1;
 					}
-					$i ++;
+					$i++;
 				}
-				$a = new String ();
-				$a->setValues ( $v );
+				
+				if($class && in_array('try-error', $class)) {
+					$a = new Error();
+				} else {
+					$a = new String();
+				}
+				$a->setValues( $v );
 				break;
 			
 			case self::XT_ARRAY_BOOL : // boolean vector
-				$n = _rserve_int32 ( $r, $i );
+				$n = _rserve_int32( $r, $i );
 				$i += 4;
 				$k = 0;
-				$vv = array ();
+				$vv = array();
 				while ( $k < $n ) {
-					$v = _rserve_int8 ( $r, $i ++ );
-					$vv [$k] = ($v == 1) ? true : (($v == 0) ? false : null);
+					$v = _rserve_int8( $r, $i ++ );
+					$vv[$k] = ($v == 1) ? true : (($v == 0) ? false : null);
 					$k ++;
 				}
-				$a = new Logical ();
-				$a->setValues ( $vv );
+				$a = new Logical();
+				$a->setValues( $vv );
 				break;
 			
 			case self::XT_RAW : // raw vector
-				$len = _rserve_int32 ( $r, $i );
+				$len = _rserve_int32( $r, $i );
 				$i += 4;
-				$v = substr ( $r, $i, $len );
-				$a = new Raw ();
-				$a->setValue ( $v );
+				$v = substr( $r, $i, $len );
+				$a = new Raw();
+				$a->setValue( $v );
 				break;
 			
 			case self::XT_ARRAY_CPLX :
-				$v = array ();
+				$v = array();
 				while ( $i < $eoa ) {
-					$real = _rserve_flt64 ( $r, $i );
+					$real = _rserve_flt64( $r, $i );
 					$i += 8;
-					$im = _rserve_flt64 ( $r, $i );
+					$im = _rserve_flt64( $r, $i );
 					$i += 8;
-					$v [] = array (
-							$real,
-							$im 
-					);
+					$v [] = array ($real, $im);
 				}
-				$a = new Complex ();
-				$a->setValues ( $v );
+				$a = new Complex();
+				$a->setValues( $v );
 				break;
 			/*
 			 * case 48: // unimplemented type in Rserve
@@ -190,10 +193,10 @@ class REXP extends Parser {
 			 */
 			default :
 				// handle unknown type
-				$a = new Unknown ( $ra );
+				$a = new Unknown( $ra );
 		}
-		if ($attr && is_object ( $a )) {
-			$a->setAttributes ( $attr );
+		if ($attr && is_object( $a )) {
+			$a->setAttributes( $attr );
 		}
 		return $a;
 	}
